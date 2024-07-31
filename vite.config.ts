956a59f39fbcca0abd31a63ea5d90b5052244e73
Kodeneko <ladysun.freedom@gmail.com>
2024-07-31 18:46:45 +0200

    [base] vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
+/// <reference types="vitest" />
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [ vue() ] });
+export default defineConfig({
+  plugins: [ vue() ],
+  test: {
+    environment: 'happy-dom',
+    globals: true
+  }
+});
