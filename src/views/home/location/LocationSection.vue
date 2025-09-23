5f9cfca82365975ade940ef1980fc37b45f18fb0
Kodeneko <ladysun.freedom@gmail.com>
2025-09-23 14:32:31 +0200

    [anim] location

--- src/views/home/location/LocationSection.vue
+++ src/views/home/location/LocationSection.vue
@@ -1,14 +1,44 @@
 <script lang="ts" setup>
 import TitleSecCompo from '@components/text/TitleSecCompo.vue';
 import AddressMapCompo from './AddressMapCompo.vue';
+import { motion as m } from "motion-v";
 </script>
 
 <template>
   <div class="location-section">
-    <TitleSecCompo :center="true">Location</TitleSecCompo>
-    <div class="address">
+    <m.div
+      :initial="{
+        opacity: 0
+      }"
+      :while-in-view="{
+        opacity: 1
+      }"
+      :inViewOptions="{ once: true, amount: 'some' }"
+      :transition="{
+        delay: 0.3,
+        duration: 0.7
+      }"
+    >
+      <TitleSecCompo :center="true">Location</TitleSecCompo>
+    </m.div>
+    <m.div 
+      class="address"
+      :initial="{
+        opacity: 0,
+        scale: 0
+      }"
+      :while-in-view="{
+        opacity: 1,
+        scale: 1
+      }"
+      :inViewOptions="{ once: true, amount: 'some' }"
+      :transition="{
+        delay: 0.3,
+        duration: 0.7
+      }"
+    >
       <AddressMapCompo />
-    </div>
+    </m.div>
   </div>
 </template>
 
