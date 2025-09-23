af950b18783df54e874872f9bc7f1368c642d4d3
Kodeneko <ladysun.freedom@gmail.com>
2025-09-23 14:23:52 +0200

    [anim] mainbar + welcome

--- src/views/home/welcome/WelcomeSec.vue
+++ src/views/home/welcome/WelcomeSec.vue
@@ -1,41 +1,71 @@
 <script lang="ts" setup>
 import BtnCompo from '@components/btn/BtnCompo.vue';
 import { socialList } from '@globals/social';
-import ramenBowl from '@assets/pics/ramen-bowl.png'
+import ramenBowl from '@assets/pics/ramen-bowl.png';
+import { motion as m } from "motion-v";
 
 </script>
 
 <template>
   <div class="welcome-sec">
-    <ejemplo />
-    <h2 class="title">Ejoy Your Ramen</h2>
-    <ul class="social">
-      <li 
-        v-for="social in socialList" 
-        :key="social.id"
-      >
-        <fai class="icon" :icon="social.icon" />
-      </li>
-    </ul>
-    <p class="desc">Lorem ipsum dolor sit amet consectetur adipiscing elit lacus eu vehicula ultricies ut, nec vestibulum neque dictumst sagittis taciti pharetra</p>
-    <div class="actions">
-      <BtnCompo 
-        label="Location"
-        icon="fa-solid fa-location-dot" 
-        size="md"
-        :line="true"
-        :full="true"
-      />
-      <BtnCompo 
-        label="Delivery"
-        icon="fa-solid fa-motorcycle" 
-        size="md"
-        :full="true"
-      />
-    </div>
-    <div class="pic">
+    <m.div 
+      class="left"
+      :initial="{
+        opacity: 0,
+        x: -100
+      }"
+      :animate="{
+        opacity: 1,
+        x: 0
+      }"
+      :transition="{
+        delay: 0.3,
+        duration: 0.7
+      }"
+    >
+      <h2 class="title">Ejoy Your Ramen</h2>
+      <ul class="social">
+        <li 
+          v-for="social in socialList" 
+          :key="social.id"
+        >
+          <fai class="icon" :icon="social.icon" />
+        </li>
+      </ul>
+      <p class="desc">Lorem ipsum dolor sit amet consectetur adipiscing elit lacus eu vehicula ultricies ut, nec vestibulum neque dictumst sagittis taciti pharetra</p>
+      <div class="actions">
+        <BtnCompo 
+          label="Location"
+          icon="fa-solid fa-location-dot" 
+          size="md"
+          :line="true"
+          :full="true"
+        />
+        <BtnCompo 
+          label="Delivery"
+          icon="fa-solid fa-motorcycle" 
+          size="md"
+          :full="true"
+        />
+      </div>
+    </m.div>
+    <m.div 
+      class="right"
+      :initial="{
+        opacity: 0,
+        x: 100
+      }"
+      :animate="{
+        opacity: 1,
+        x: 0
+      }"
+      :transition="{
+        delay: 0.3,
+        duration: 0.7
+      }"
+    >
       <img :src="ramenBowl" />
-    </div>
+    </m.div>
   </div>
 </template>
 
@@ -43,29 +73,26 @@ import ramenBowl from '@assets/pics/ramen-bowl.png'
 @import url('global.less');
 
 .welcome-sec {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: auto auto auto auto;
+  display: flex;
   padding: 2.5rem;
   padding-bottom: 8.75rem;
-  row-gap: 1.25rem;
-  grid-template-areas: 
-    "title pic"
-    "social pic"
-    "desc pic"
-    "actions pic"
-  ;
-  
+  gap: 1.25rem;
+}
+
+.welcome-sec .left {
+
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+
   .title {
-    grid-area: title;
     max-width: 33.13rem;
     font-size: 4.5rem;
     font-weight: bold;
-    padding-bottom: 2.5rem;
+    margin-bottom: 2.5rem;
   }
 
   .social {
-    grid-area: social;
     display: flex;
     gap: 1.25rem;
 
@@ -83,29 +110,26 @@ import ramenBowl from '@assets/pics/ramen-bowl.png'
   }
 
   .desc {
-    grid-area: desc;
     max-width: 33.13rem;
-    font-size: 2rem;
-    padding-bottom: 1.25rem;
+    font-size: 1.75rem;
   }
 
   .actions {
-    grid-area: actions;
     display: flex;
     gap: 1.25rem;
   }
+}
 
-  .pic {
-    grid-area: pic;
-    align-self: end;
-    justify-self: end;
+.welcome-sec .right {
+    flex-grow: 1;
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-end;
 
-    img {
-      height: 25rem;
-      transform: rotateY(180deg);
-    }
+  img {
+    height: 25rem;
+    transform: rotateY(180deg);
   }
-
 }
 
 @media @mediaTablet, @mediaMobile {
