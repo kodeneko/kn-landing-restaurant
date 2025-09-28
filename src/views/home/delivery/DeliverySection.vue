563c8fbcbb1e86e9ef9229bbb2253667a62f1f38
Kodeneko <ladysun.freedom@gmail.com>
2025-09-28 17:57:30 +0200

    [dev] anim motorbike

--- src/views/home/delivery/DeliverySection.vue
+++ src/views/home/delivery/DeliverySection.vue
@@ -19,6 +19,12 @@ const sizeBtns = () => isMobile ? 'sm' : 'md'
 <template>
   <div class="delivery-section" ref="sectionRef">
     <div class="left">
+      <div class="shade"></div>
+      <div class="line-cont">
+        <div class="line01 line"></div>
+        <div class="line02 line"></div>
+        <div class="line03 line"></div>
+      </div>
       <img 
         class="pic" 
         :src="pic" 
@@ -54,10 +60,75 @@ const sizeBtns = () => isMobile ? 'sm' : 'md'
   justify-content: center;
 
   .left {
-
+    position: relative;
     .pic {
       max-width: 480px;
       width: 100%;
+
+      animation-name: levitation;
+      animation-duration: 1s;
+      animation-iteration-count: infinite;
+      animation-direction: alternate;
+      animation-timing-function: linear;
+    }
+
+    .shade {
+      position: absolute;
+      bottom: 0;
+      left: calc(50% - 8.5rem);
+      width: 20rem;
+      height: 1rem;
+      border-radius: 50%;
+      border: none;
+      background-color: @c-dark-trans;
+      box-shadow: 0 0 0.3rem 0.3rem @c-dark-trans;
+      opacity: 0.7;
+
+      animation-name: shadow;
+      animation-duration: 1s;
+      animation-iteration-count: infinite;
+      animation-direction: alternate;
+      animation-timing-function: linear;
+    }
+
+    .line-cont {
+      position: absolute;
+      top: 40%;
+      left: -30rem;
+    }
+
+    .line {
+      position: relative;
+      width: 30rem;
+      height: 0.3rem;
+      background-color: @c-white-trans;
+      border-radius: 50%;
+
+      animation-name: lines;
+      animation-duration: 0.7s;
+      animation-iteration-count: infinite;
+      animation-direction: normal;
+      animation-timing-function: linear;
+    }
+
+    .line01 {
+      position: relative;
+      top: -3rem;
+      left: 0rem;
+    }
+
+    .line02 {
+      position: relative;
+      top: 1rem;
+      left: 10rem;
+      animation-delay: -2s;
+    }
+
+    .line03 {
+      position: relative;
+      top: 6rem;
+      left: 4rem;
+      animation-delay: -4s;
     }
   }
 
@@ -99,4 +170,34 @@ const sizeBtns = () => isMobile ? 'sm' : 'md'
 
   }
 } 
+
+@keyframes levitation {
+  0% {
+    transform: translateY(-0.7rem) skewX(-1deg);
+  }
+  100% {
+    transform: translateY(0rem) skewX(0deg);
+  }
+}
+
+@keyframes shadow {
+  0% {
+    transform: scale(0.8);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes lines {
+  0% {
+    transform: translate(0, 0);
+    opacity: 1;
+  }
+
+  100% {
+    transform: translate(20rem, 0);
+    opacity: 0;
+  }
+}
 </style>
