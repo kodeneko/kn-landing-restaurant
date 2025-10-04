b3dde42813a8b116186eee3542f0f2d440201280
Kodeneko <ladysun.freedom@gmail.com>
2025-10-04 17:37:42 +0200

    [dev] Menu side menucard

--- src/components/menu/MenuNavCompo.vue
+++ src/components/menu/MenuNavCompo.vue
@@ -1,5 +1,8 @@
 <script lang="ts" setup>
 import { Opt } from '@models/user';
+import { motion as m } from "motion-v";
+import { onMounted } from 'vue';
+import { reactive, ref } from 'vue';
 
 defineProps<{
   selected: Opt;
@@ -8,26 +11,54 @@ defineProps<{
 defineEmits<{
   click: [opt: Opt]
 }>();
+
+const contRef = ref(null);
+const constraints = reactive({ left: 0, right: 0 });
+
+onMounted(() => {
+  if (contRef.value) {
+    const cont = contRef.value as HTMLElement;
+    const widthVisible = cont.clientWidth;
+    const widthHidden = cont.scrollWidth;
+    constraints.left = -1 *(widthHidden - widthVisible + 50);
+    constraints.right = 0;
+  }
+});
 </script>
 
 <template>
-  <ul class="menu-main">
-    <li
-      v-for="opt in opts"
-      :key="opt.id"
-      :on-click="$emit('click', opt)"  
+  <div 
+    class="outer"
+    ref="contRef"
+  >
+    <m.ul 
+      class="menuMain"
+      :drag="'x'"
+      :drag-constraints="constraints"
+      :drag-momentum="false"
+      :while-tap="{ cursor: 'grabbing' }"
     >
-      <RouterLink 
-        :class="{opt: true, selected: selected.id === opt.id}"
-        :to="opt.href as string" 
-      >{{ opt.label }}
-      </RouterLink>
-    </li>
-  </ul>
+      <li
+        v-for="opt in opts"
+        :key="opt.id"
+        :on-click="$emit('click', opt)"  
+      >
+        <RouterLink 
+          :class="{opt: true, selected: selected.id === opt.id}"
+          :to="opt.href as string" 
+        >{{ opt.label }}
+        </RouterLink>
+      </li>
+    </m.ul>
+  </div>
 </template>
 
 <style lang="less" scoped>
-.menu-main {
+.outer {
+  overflow: hidden;
+}
+
+.menuMain {
   display: flex;
   gap: 20px;
   .opt {
